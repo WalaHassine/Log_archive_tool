139b0c86a973adf063f13cf359504ae502796867
Wala Hassine <Walahassine2002@gmail.com>
2026-02-06 18:24:49 +0100

    implementing the log archiving process (shell approache)

--- Shell/Log.sh
+++ Shell/Log.sh
@@ -20,7 +20,12 @@ read -r -p "Please select an option (1-5): " option
 case $option in
     1)
         log_directory=$(input_prompt "Enter the log directory" "/var/log")
-        echo "Log directory set to: $log_directory"
+        if [ ! -d "$log_directory" ]; then
+                echo "Error: Log directory does not exist."
+                log_directory=""
+        else
+                echo "Log directory set to $log_directory"
+        fi
         ;;
     2)
         days_to_keep=$(input_prompt "Enter the number of days to keep logs" "30")
@@ -33,6 +38,29 @@ case $option in
     4)
         echo "Running the log archiving process..."
         # Here you would call your log archiving function or script
+        if [ -z $log_directory ]; then
+            echo "Error: Log directory is not set. Please specify it first."
+        else
+
+        #archive directory
+        archive_directory="$log_directory/archives"
+        mkdir -p "$archive_directory"
+
+        #archive name with timestamp
+        timestamp =$(date +"%Y%m%d%H%M%S")
+        archive_file="$archive_directory/logs_$timestamp.tar.gz"
+
+        #find and compress logs older than specified days (days to keep)
+        find "$log_directory" -type f -mtime +$days_to_keep print0 | tar -czvf "$archive_file" --null -T -
+        echo "Logs older than $days_to_keep days have been archived to $archive_file" >> "$archive_directory/archive_log.txt"
+        #find and delete logs older than the specified days (days to keep)
+        find "$log_directory" -type f -mtime +$days_to_keep -exec rm {} \;
+        echo "Logs older than $days_to_keep days have been deleted from $log_directory"
+        echo "Log archiving process completed. Archive file: $archive_file"
+        #find and delete backup archives older than the specified days (backup days to keep)
+        find "$archive_directory"  -type f -name "*.tar.gz" -mtime +$backup_days_to_keep -exec rm {} \;
+        echo "Backup archives older than $backup_days_to_keep days have been deleted from $archive_directory"
+        fi
         ;;
     5)
         echo "Exiting..."
@@ -42,3 +70,4 @@ case $option in
         echo "Invalid option. Please select a number between 1 and 5."
         ;;
 esac
+done
